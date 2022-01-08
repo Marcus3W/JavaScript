1d037a240ad80f0d29fb7e315992ff371df6be6b
Marc Wheeler <60431130+Marcus3W@users.noreply.github.com>
2022-01-08 10:25:47 +0000

    NavBar completed

--- freeCodeCamp/Vanilla JavaScript Projects/4-NavBar/setup/app.js
+++ freeCodeCamp/Vanilla JavaScript Projects/4-NavBar/setup/app.js
@@ -3,3 +3,26 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function() {
+    //console.log(links.classList);
+    //console.log(links.classList.contains("random")); // returns false as class doesnt exist
+    //console.log(links.classList.contains("links")); // returns true as class doesnt exist
+
+    /*LONG METHOD - with add / remove
+    if(links.classList.contains("show-links")) {
+        links.classList.remove("show-links");
+    }
+    else {
+        links.classList.add("show-links");
+    }
+    */
+
+    // SHORT METHOD - with toggle
+    links.classList.toggle("show-links");
+});
+
+
