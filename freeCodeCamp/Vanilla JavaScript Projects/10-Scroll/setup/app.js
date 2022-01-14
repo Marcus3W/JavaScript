ca123eb2e00e00fd4225d90400a24823a29be415
Marcus3W <marcus3w@live.com>
2022-01-14 21:43:07 +0000

    freeCodeCamp - JS - scroll update

--- freeCodeCamp/Vanilla JavaScript Projects/10-Scroll/setup/app.js
+++ freeCodeCamp/Vanilla JavaScript Projects/10-Scroll/setup/app.js
@@ -4,8 +4,23 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const date = document.getElementById("date");
+date.innerText = new Date().getFullYear();
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+    //linksContainer.classList.toggle('show-links');
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const linksHeight = links.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+        linksContainer.style.height = `${linksHeight}px`;
+    } else {
+        linksContainer.style.height = 0;
+    }
+});
 
 // ********** fixed navbar ************
 
